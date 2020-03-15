e128b576ed6092fef112b7dbcee280c6271fcf71
gabrielccarvalho <caampos22@gmail.com>
2020-03-15 03:39:06 -0300

    chore: database config

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
 
