39c26c6b6f044da816a45cd7110f5b4f5f54ab16
gabrielccarvalho <caampos22@gmail.com>
2020-03-20 01:38:42 -0300

    fix: removed hardDelete method

--- src/app/controllers/RaffleController.js
+++ src/app/controllers/RaffleController.js
@@ -122,26 +122,6 @@ class RaffleController {
     return res.json(raffle);
   }
 
-  async hardDelete(req, res) { // Not recomended to use
-    const raffle = await Raffle.findOne({ where: { id: req.params.id } });
-
-    if (raffle.user_id !== req.userId) {
-      return res.status(401).json({
-        error: "You don't have permission to delete this raffle",
-      });
-    }
-
-    if (!raffle) {
-      return res.status(404).json({
-        error: "You don't have a raffle with the provided id",
-      });
-    }
-
-    raffle.destroy();
-
-    return res.json(raffle);
-  }
-
   async draw(req, res) {
     const raffle = await Raffle.findOne({ where: { id: req.params.id } });
 
